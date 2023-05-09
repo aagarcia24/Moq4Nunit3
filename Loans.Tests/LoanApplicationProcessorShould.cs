dfdbfedbb7727b886ea0a842df2689c93585e682
Alvaro Garcia <aagarcia24@hotmail.com>
2023-05-09 16:39:11 -0500

    module 2

---
+++ Loans.Tests/LoanApplicationProcessorShould.cs
@@ -0,0 +1,39 @@
+﻿using Loans.Domain.Applications;
+using Moq;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    public class LoanApplicationProcessorShould
+    {
+        [Test]
+        public void DeclineLowSalary()
+        {
+            LoanProduct product = new LoanProduct(99, "Loan", 5.25m);
+            LoanAmount amount = new LoanAmount("USD", 200_000m);
+            var application = new LoanApplication(42, product, amount, "Sarah", 25, "133 Pluralsight Drive, Draper, Utah", 64_999m);
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+            var mockCreditScorer = new Mock<ICreditScorer>();
+            var sut = new LoanApplicationProcessor(mockIdentityVerifier.Object, mockCreditScorer.Object);
+
+            sut.Process(application);
+
+            Assert.That(application.GetIsAccepted(), Is.False);
+        }
+
+        [Test]
+        public void Accept()
+        {
+            LoanProduct product = new LoanProduct(99, "Loan", 5.25m);
+            LoanAmount amount = new LoanAmount("USD", 200_000m);
+            var application = new LoanApplication(42, product, amount, "Sarah", 25, "133 Pluralsight Drive, Draper, Utah", 65_000m);
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+            var mockCreditScorer = new Mock<ICreditScorer>();
+            var sut = new LoanApplicationProcessor(mockIdentityVerifier.Object, mockCreditScorer.Object);
+
+            sut.Process(application);
+
+            Assert.That(application.GetIsAccepted(), Is.True);
+        }
+    }
+}
